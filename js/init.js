38e1b5aa2ab95aba24d94beeb2529085b86a556f
IreneGlezGa <ireneglezga13@gmail.com>
2021-11-28 17:10:19 -0600

    Agregue los archivos de chat

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDzubTbUFkRzeAGY5gX0uW0BYAIeMy1sOA",
+  authDomain: "fbauten-c08e8.firebaseapp.com",
+  projectId: "fbauten-c08e8",
+  storageBucket: "fbauten-c08e8.appspot.com",
+  messagingSenderId: "551630226284",
+  appId: "1:551630226284:web:bc83d11fe7c8e50878e949",
+  measurementId: "G-4BKCR6GLE4"
+    });
